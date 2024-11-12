2d0dd79250ee66cb1691edf4f407370f8d4bb58f
Teja <teja090205@gmail.com>
2024-11-12 10:40:07 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int a=sc.nextInt();
+        int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(c>a+b || a>b)
+                System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
